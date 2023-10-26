28522f2fb7380a8263d730b70269927aa99ee8d0
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2023-10-26 19:46:20 +0200

    feat: export defineConfig

---
+++ packages/integration/config.ts
@@ -0,0 +1,78 @@
+import type { AstroUserConfig } from "astro/config";
+import type { GlobalIntegrationOptions } from "./types";
+import type { AstroConfig } from "astro";
+import { githubDataIntegration } from "./integrations/github-data";
+import { getIntegrationName } from "./utils";
+import netlify from "@astrojs/netlify/functions";
+import { vitePluginVirtualImports } from "./integrations/virtual-imports";
+import { npmDataIntegration } from "./integrations/npm-data";
+import tailwind from "@astrojs/tailwind";
+
+export const defineConfig = (
+  options: GlobalIntegrationOptions
+): AstroUserConfig => {
+  return {
+    site: "https://choose-tech.com",
+    srcDir: ".",
+    base: options.base,
+    trailingSlash: "always",
+    output: "server",
+    adapter: netlify(),
+    vite: {
+      build: {
+        rollupOptions: {
+          external: ["@astrojs/netlify/netlify-functions.js"],
+        },
+      },
+      ssr: {
+        noExternal: [
+          "@fontsource-variable/plus-jakarta-sans",
+          "@fontsource-variable/jetbrains-mono",
+        ],
+        external: ["@choose-tech/collections"],
+      },
+    },
+    integrations: [
+      tailwind({
+        configFile: "node_modules/@choose-tech/astro/tailwind.ts",
+        applyBaseStyles: false,
+      }),
+      {
+        name: getIntegrationName("global"),
+        hooks: {
+          "astro:config:setup": async (...args) => {
+            const { config, injectRoute, updateConfig } = args[0];
+
+            injectRoute({
+              pattern: "/",
+              entryPoint: "@choose-tech/astro/pages/index.astro",
+              prerender: false,
+            });
+
+            injectRoute({
+              pattern: "/api/v1/categories.json",
+              entryPoint: "@choose-tech/astro/pages/api/v1/categories.json.ts",
+              prerender: true,
+            });
+
+            injectRoute({
+              pattern: "/api/v1/libraries.json",
+              entryPoint: "@choose-tech/astro/pages/api/v1/libraries.json.ts",
+              prerender: true,
+            });
+
+            const newConfig: Partial<AstroConfig> = {
+              vite: {
+                plugins: [vitePluginVirtualImports(options, config)],
+              },
+            };
+
+            updateConfig(newConfig);
+          },
+        },
+      },
+      githubDataIntegration(),
+      npmDataIntegration(),
+    ],
+  };
+};
--- playground/astro.config.mjs
+++ playground/astro.config.mjs
@@ -1,15 +1,10 @@
-import { defineConfig } from "astro/config";
-import chooseTechIntegration from "@choose-tech/astro";
+import { defineConfig } from "@choose-tech/astro/config";
 
 export default defineConfig({
-  integrations: [
-    chooseTechIntegration({
-      base: "/js-libs",
-      title: "JS Libraries",
-      description: "Hand-picked comparisons for your next project.",
-      color: "#F7DF1E",
-      repo: "choose-tech/astro-integration",
-      logoFilename: "logo.svg",
-    }),
-  ],
+  base: "/js-libs",
+  title: "JS Libraries",
+  description: "Hand-picked comparisons for your next project.",
+  color: "#F7DF1E",
+  repo: "choose-tech/astro-integration",
+  logoFilename: "logo.svg",
 });
