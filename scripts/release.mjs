dec5553e31a36e3db4d9015cad462ff9bd001507
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2023-11-04 16:08:27 +0100

    chore: fix script

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -33,35 +33,42 @@ const asyncExec = promisify(exec);
 const bumpVersion = (version, index) => {
   const x = version.split(".");
 
-  x[index]++
+  x[index]++;
 
   return x.join(".");
 };
 
 (async () => {
-  /** @type {string} */
-  let answer;
-  while (!["0", "1", "2"].includes(answer)) {
-    answer = await askQuestion("[0] Major ; [1] Minor ; [2] Patch : ");
+  try {
+    /** @type {string} */
+    let answer;
+    while (!["0", "1", "2"].includes(answer)) {
+      answer = await askQuestion("[0] Major ; [1] Minor ; [2] Patch : ");
+    }
+
+    const packagePath = path.resolve(
+      process.cwd(),
+      "./packages/integration/package.json"
+    );
+    const pkg = JSON.parse(await fsp.readFile(packagePath, "utf-8"));
+
+    const version = bumpVersion(pkg.version, parseInt(answer));
+
+    pkg.version = version;
+
+    await fsp.writeFile(packagePath, JSON.stringify(pkg, null, 2), "utf-8");
+
+    await asyncExec("git add .");
+    await asyncExec(`git commit -m @choose-tech/astro@${version}`);
+    await asyncExec("git push");
+
+    process.chdir("./packages/integration");
+
+    await asyncExec("pnpm publish");
+
+    console.info(`[SUCCESS] ${version} has been published on NPM`);
+  } catch (err) {
+    console.warn(`[ERROR] An error occured`);
+    console.error(err);
   }
-
-  const packagePath = path.resolve(
-    process.cwd(),
-    "./packages/integration/package.json"
-  );
-  const pkg = JSON.parse(await fsp.readFile(packagePath, "utf-8"));
-
-  const version = bumpVersion(pkg.version, parseInt(answer));
-
-  pkg.version = version;
-
-  await fsp.writeFile(packagePath, JSON.stringify(pkg, null, 2), "utf-8");
-
-  // await asyncExec("git add .");
-  // await asyncExec(`git commit -m @choose-tech/astro@${version}`);
-  // await asyncExec("git push");
-
-  // process.chdir("./packages/integration")
-
-  // await asyncExec("pnpm publish")
 })();
