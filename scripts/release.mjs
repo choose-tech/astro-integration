ecbf92b7a005acf919626e4cb09329033683dc74
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2023-11-04 12:27:59 +0100

    chore: improve release script

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -28,19 +28,14 @@ const asyncExec = promisify(exec);
 /**
  *
  * @param {string} version
- * @param {"major" | "minor" | "patch"} version
+ * @param {number} index
  */
-const bumpVersion = (version, type) => {
-  const [major, minor, patch] = version.split(".");
-  const o = {
-    major: parseInt(major),
-    minor: parseInt(minor),
-    patch: parseInt(patch),
-  };
+const bumpVersion = (version, index) => {
+  const x = version.split(".");
 
-  o[type]++;
+  x[index]++
 
-  return [o.major, o.minor, o.patch].join(".");
+  return x.join(".");
 };
 
 (async () => {
@@ -50,25 +45,23 @@ const bumpVersion = (version, type) => {
     answer = await askQuestion("[0] Major ; [1] Minor ; [2] Patch : ");
   }
 
-  const bumpType = { 0: "major", 1: "minor", 2: "patch" }[answer];
-
   const packagePath = path.resolve(
     process.cwd(),
     "./packages/integration/package.json"
   );
   const pkg = JSON.parse(await fsp.readFile(packagePath, "utf-8"));
 
-  const version = bumpVersion(pkg.version, bumpType);
+  const version = bumpVersion(pkg.version, parseInt(answer));
 
   pkg.version = version;
 
   await fsp.writeFile(packagePath, JSON.stringify(pkg, null, 2), "utf-8");
 
-  await asyncExec("git add .");
-  await asyncExec(`git commit -m @choose-tech/astro@${version}`);
-  await asyncExec("git push");
+  // await asyncExec("git add .");
+  // await asyncExec(`git commit -m @choose-tech/astro@${version}`);
+  // await asyncExec("git push");
 
-  process.chdir("./packages/integration")
+  // process.chdir("./packages/integration")
 
-  await asyncExec("pnpm publish")
+  // await asyncExec("pnpm publish")
 })();
